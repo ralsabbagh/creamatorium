f1a36bc149b07aa7d80e4fa898bc9a532d9ad3e8
kholdunn <k.naqawa@tap.company>
2019-12-07 03:20:58 +0300

    push

--- src/components/orderCategory/orderCategory.js
+++ src/components/orderCategory/orderCategory.js
@@ -8,7 +8,7 @@ class OrderCategory extends Component {
         return this.props.categoryOptions.map((categoryOption, index) => {
             return (
                 <div key={index} className="categoryOption">
-                    <input className="optionCheckBox" type="checkbox" name={categoryOption} value={categoryOption}></input>
+                    <input className="optionCheckBox" type="checkbox" name={categoryOption} value={categoryOption} checked = {this.props.optionsSelections[index]} ></input>
                     <h5 className="optionCheckTitle">{categoryOption}</h5>
                 </div>
             );
